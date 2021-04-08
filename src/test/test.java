e78e8ca7de28ab59ba10c6ff495c9376c3312dee
EomT <EomT@DESKTOP-9JF2160>
2021-04-08 10:36:46 +0900

    test

---
+++ src/test/test.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		VO vo =new VO();
+		vo.setA("B");
+		System.out.println(vo.getA());
+		List<VO> alist=new ArrayList<VO>();
+
+	}
+
+}
